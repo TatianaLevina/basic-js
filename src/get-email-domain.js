4cabcca4260ef3dd330d58d58fd6a2c58025db76
Tatiana Levina <tatiana.levina.v@gmail.com>
2023-04-18 01:01:53 +0300

    feat: transform array done

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -11,7 +11,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
