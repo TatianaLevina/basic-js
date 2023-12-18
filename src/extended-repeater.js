22e252e01d47588d136621f06785a5b051ca0e40
Tatiana Levina <tatiana.levina.v@gmail.com>
2023-12-18 23:44:39 +0300

    feat: solution of extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
+function repeater(str, options) {
+  let resStr = "";
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = "|";
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.addition !== undefined) {
+    let addition = `${options.addition}`
+    resStr = resStr + addition + options.additionSeparator;
+    resStr = resStr.repeat(options.additionRepeatTimes);
+    resStr = resStr.slice(0, resStr.length - options.additionSeparator.length);
+  }
+  resStr = `${str}${resStr}${options.separator}`;
+  resStr = resStr.repeat(options.repeatTimes);
+  resStr = resStr.slice(0, resStr.length - options.separator.length);
+  return resStr;
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
