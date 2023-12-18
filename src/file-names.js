7d4347f2c88c23cb0b23975e27160d68223e4cdc
Tatiana Levina <tatiana.levina.v@gmail.com>
2023-12-18 11:50:32 +0300

    feat: solution of email domain

--- src/file-names.js
+++ src/file-names.js
@@ -30,7 +30,6 @@ function renameFiles(names) {
       }
     }
   }
-  console.log(arr);
   return arr;
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
